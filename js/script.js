0990ec43634ef9f22e3d0eec52bccc3ee6e50955
VLuca997 <kulertoast997@gmail.com>
2023-06-17 16:18:30 +0200

    Creazione html+ css + array carousel

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// creo un Array con le 5 immagini per il carosello
+
+const imagesCarousel = [
+    "img/01.webp",
+    "img/02.webp",
+    "img/03.webp",
+    "img/04.webp",
+    "img/05.webp"
+];
+
