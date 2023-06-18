bd862089ea017af1a1f128ae179972969d388ce8
VLuca997 <kulertoast997@gmail.com>
2023-06-19 00:49:03 +0200

    aggiunti Event Listner button e ciclo for img

--- js/script.js
+++ js/script.js
@@ -13,3 +13,29 @@ const imagesCarousel = [
     "img/04.webp",
     "img/05.webp"
 ];
+
+//seleziono gli elementi html del carosello e dei bottoni
+let carouselDiv = document.querySelector('.carousel');
+let arrowUp = document.querySelector('.arrow-up');
+let arrowDown = document.querySelector('.arrow-down');
+
+
+// Ciclo for per generare il markup delle immagini nel carosello
+for ( let i = 0; i < imagesCarousel.length; i++){
+    let img = document.createElement('img'); // creo un elemento img
+    img.classList('carousel-img'); //gli attribuiscoi la classe carousel-img
+
+    if (i === 0){ // Se l'indice è 0
+        img.classList.add('active'); // Aggiungiamo anche la classe active per rendere l'elemento visibile
+    }
+    img.src = imagesCarousel[i]; // dico all'elemento src che l'indice di questa immagine corrisponde all'elemento creato.
+    carouselDiv.appendChild(img); // aggiungo questo elemento come figlio di carousel.img
+}
+
+// Creo gli event listner per i button
+arrowUp.addEventListener('click', scrollUp);
+arrowDown.addEventListener('click',scrollDown);
+
+//funzione per il click dei button, in modo che quando si arriva alal fine dell'array si ricominci da capo.
+
+//funzione per scrollUp
