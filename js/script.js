2ef81bdfa915ef99fda03cb1759b82a77ddf2b62
VLuca997 <kulertoast997@gmail.com>
2023-06-19 00:32:09 +0200

    Ragionamento per JS

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 // creo un Array con le 5 immagini per il carosello
+// Inizializzo le variabili per : carosello, frecce up-down.
+//gengero un ciclo for per il markup delle immagini all'intero del carosello.
+//genero 2 event listner per ilclick dei pulsanti
+//genero 2 funzioni per l'evento click dei pulsanti, per dargli la funzione active e per far si che il carosello, alla fine dell'array ricominci da indice 0.
 
+
+//Array Carosello immagini
 const imagesCarousel = [
     "img/01.webp",
     "img/02.webp",
@@ -7,4 +13,3 @@ const imagesCarousel = [
     "img/04.webp",
     "img/05.webp"
 ];
-
